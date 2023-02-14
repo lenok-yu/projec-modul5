308cb7b55d417b39c95cba412887516a3203fe62
lenok-yu <enyudina@gmail.com>
2023-02-14 13:56:12 +1100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import { events } from './data';
 import './App.css';
 
 function App() {
+
+  const [animal, setAnimal] = useState(0);
+  const {id, name, description, photo, showMore} = data[animal];
+
+  const previousAnimal = () => {
+    setAnimal((animal => {
+      animal--;
+      if (animal < 0) animal = data.length - 1;
+      return animal;
+    }))
+  }
+  const nextAnimal = () => {
+    setAnimal((animal => {
+      animal++;
+      if (animal > data.length - 1) animal = 0;
+      return animal;
+    }))
+  }
+
+  const [showText, setShowText] = useState(false);
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore;
+    setShowText(!showText);
+  }
+
+  const [myEvents,setMyEvents] = useState(events);
+  
+  const removeEvent = (id) => {
+    let newEvents = myEvents.filter(event => event.id !== id);
+    setMyEvents(newEvents);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My Melbourne </h1>
+      <div className='btn'>
+          <button onClick={previousAnimal}> Previous </button>
+          <h2> Animals in the city</h2>
+          <button onClick={nextAnimal}> Next </button>
+      </div>
+      <div className='container'>
+        <div>
+          <h3> {id}. {name} </h3>
+        </div>
+        <div>
+          <img src={photo} width='300px' alt='animal'/>
+        </div>
+        <div>
+          <p> 
+            {showMore ? description : description.substring(0,120) + "..."}
+            <button className='btnShowMore' onClick={() => showTextClick(data[animal])}>{showMore ? "Show less" : "Show more"}</button>
+          </p>
+        </div>
+      </div>
+
+      <div className='container1'>
+        <h2>List of {myEvents.length} events</h2>
+        {myEvents.map( item => {
+          const {id, event, place, day} = item;
+          return(
+            <div key={id} className="item">
+              <div>
+                <h3> {id}. {event} </h3>
+                <p> {place} </p>
+                <p> {day} </p>
+              </div>
+              <div>
+                <button className='btn1' onClick={() => removeEvent(id)}>Remove</button>
+              </div>
+            </div>
+            
+          )
+        })}
+        <div>
+          <button className='btn1' onClick={() => setMyEvents([])}> Delete all</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,64 @@
+
+export const data = [
+    {
+        id: 1,
+        name: "Flying fox",
+        description: "Flying foxes eat fruit and other plant matter, and occasionally consume insects as well. They locate resources with their keen sense of smell. Most, but not all, are nocturnal. They navigate with keen eyesight, as they cannot echolocate. ",
+        photo: "https://images.unsplash.com/photo-1566803374012-0b939a58a437?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zmx5aW5nJTIwZm94fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1200&q=60",
+        showMore: false
+    },
+    {
+        id: 2,
+        name: "Rainbow lorikeet",
+        description: "The rainbow lorikeet (Trichoglossus moluccanus) is a species of parrot found in Australia. Rainbow lorikeets feed mainly on fruit, pollen and nectar, and possess a tongue adapted especially for their particular diet.",
+        photo: "https://images.unsplash.com/photo-1582068019392-4b7eb29b8609?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9yaWtlZXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1200&q=60",
+        showMore: false
+    },
+    {
+        id: 3,
+        name: "Common brushtail possum",
+        description: "Like most possums, the common brushtail possum is nocturnal. It is mainly a folivore, but has been known to eat small mammals such as rats. In most Australian habitats, eucalyptus leaves are a significant part of the diet, but rarely the sole item eaten.",
+        photo: "https://images.unsplash.com/photo-1646475692848-6a8b095d2cbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBvc3N1bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60",
+        showMore: false
+    },
+    {
+        id: 4,
+        name: "Australian magpie",
+        description: "The Australian magpie (Gymnorhina tibicen) is a black and white passerine bird native to Australia and southern New Guinea. ",
+        photo: "https://images.unsplash.com/photo-1668921707749-f0447f9c09a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8QXVzdHJhbGlhbiUyMG1hZ3BpZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60",
+        showMore: false
+    },
+    {
+        id: 5,
+        name: "Spiders",
+        description: "The Pholcidae are a family of araneomorph spiders. The family contains over 1,800 individual species of pholcids, including those commonly known as cellar spider, daddy long-legs spider, carpenter spider, daddy long-legger, vibrating spider, gyrating spider, long daddy, and skull spider. The family, first described by Carl Ludwig Koch in 1850, is divided into 94 genera.",
+        photo: "https://images.unsplash.com/photo-1632681824188-9168f7881b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGFkZHklMjBsb25nJTIwbGVncyUyMHNwaWRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60",
+        showMore: false
+    }
+]
+export const events = [
+    {
+        id: 1,
+        event: "Time. Rone",
+        place: "Flinder Street Station level 3",
+        day: "Sun, Mon, Wed 10-6pm "
+    },
+    {
+        id: 2,
+        event: "Ballet Under the Stars",
+        place: "Sidney Myer Music Bowl",
+        day: "Saturday 25 February"
+    },
+    {
+        id: 3,
+        event: "Harry Potter and the Cursed Child",
+        place: "Princess Theater, Melbourne",
+        day: "Until 9 July 2023"
+    },
+    {
+        id: 4,
+        event: "Frozen in Concert",
+        place: "The Plenary, MCEC, Melbourne",
+        day: "17 July 2023"
+    }
+]
